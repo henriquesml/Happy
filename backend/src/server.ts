6348782b448eae31b88e6e863d8893f2dfb0d58d
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-10-14 20:54:48 -0300

    adição de ORM e conexão com sqlite

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,39 @@
 import express from "express";
+import { getRepository } from "typeorm";
+
+import Orphanage from "./models/Orphanage";
+
+import "./database/connection";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({msg: "OK"})
+app.post("/orphanages", async (req, res) => {
+  const orphanageRepository = getRepository(Orphanage)
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = req.body;
+
+  const orphanage = orphanageRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  })
+
+  await orphanageRepository.save(orphanage);
+
+  return res.status(200).json(orphanage)
 })
 
 app.listen(3333);
